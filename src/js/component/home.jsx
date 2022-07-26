59f10d3e1e90ca33e63c49730005245182022e0a
dudubalack <dominguesdatrindadecarloseduar@gmail.com>
2022-07-26 20:18:50 +0000

    case listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
+import React, { useRef, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+const [tasks, setTasks] = useState([]) 
+	const inputRef = useRef();
+
+	const onAddButtonClick = (e) => {
+		e.preventDefault();
+
+		const taskTitle = inputRef.current.value;
+		 if (taskTitle === ""){return} 
+		setTasks([...tasks, taskTitle]); 
+		inputRef.current.value = ""; 
+	}
+	const onDeleteButtonClick = (position) => { 
+		tasks.splice(position,1); 
+		setTasks([...tasks])
+	}
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+        <div className="container">
+            <h1>Todos</h1> 
+            <form onSubmit={onAddButtonClick} className="input-group mb-3">
+                <input ref={inputRef} type="text" className="form-control" aria-label="Text input with segmented dropdown button" />
+            </form>
+
+            {tasks.length === 0
+            ? (<span>No hay tareas, añadir tareas</span>) 
+            : tasks.map((taskElement, i) => { 
+                return ( <li key ={i}>{taskElement}<button type="button" className="btn-close" aria-label="Delete" onClick={() => onDeleteButtonClick(i)}>X</button></li> ) 
+              })
+            }
+        </div>
+    );
 };
 
 export default Home;
+
+
